61d82d967853251525079499ade158d2fc482a60
mattwardpictures <mattwardpictures@gmail.com>
2018-10-25 13:45:03 -0400

    added another line

--- src/snippet/MainApp.java
+++ src/snippet/MainApp.java
@@ -5,7 +5,8 @@ public class MainApp {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Goodbye, cruel world.");
-		
+		System.out.println("So long and thanks for all the fish!");
+
 	}
 
 }
